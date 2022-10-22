c479c795fdb55ed9106e2403a5e61d181deac766
Alberth Nils <alberth.castillo@tecsup.edu.pe>
2022-10-22 18:36:35 -0500

    EVIDENCIA

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductosComponent } from './productos/productos.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductosComponent,
+    AcercadeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
