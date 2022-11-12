b32b18fccd63531b013ab8d79a9f5d5a6d0dd05d
alberth-contn <alberth.castilloqtecsup.edu.pe>
2022-11-11 19:36:21 -0500

    Mi segundo examen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exma';
+  title = 'eventohijopadre';
+  usuarios: string[] = [];
+
+  actualizar(t: string) {
+    this.usuarios.push(t);
+  }
 }
